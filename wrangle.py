7c534d7a8a8b22c72a2db84e1b95ee32aaa044d5
“Brandon <brandon.j.bryant1@gmail.com>
2021-04-11 16:47:50 -0500

    changed stratification

--- wrangle.py
+++ wrangle.py
@@ -2,15 +2,25 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer, KNNImputer
+from sklearn.metrics import mean_squared_error, r2_score, explained_variance_score
+from sklearn.linear_model import LinearRegression
+from sklearn.feature_selection import f_regression, RFE, SelectKBest
+import sklearn.preprocessing
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
 
 def get_wine_data():
     '''
-    Grab our data from path and read as dataframe
+    Grab the wine data from file path and read as a dataframe
     '''
     
     df = pd.read_csv('wine_quality.csv')
     
     return df
+
+
 
 
 def missing_zero_values_table(df):
@@ -33,6 +43,10 @@ def missing_zero_values_table(df):
           " columns that have NULL values.")
     return mz_table
 
+
+
+
+
 def handle_missing_values(df, prop_required_column = .5, prop_required_row = .75):
     threshold = int(round(prop_required_column*len(df.index),0))
     df.dropna(axis=1, thresh=threshold, inplace=True)
@@ -40,6 +54,23 @@ def handle_missing_values(df, prop_required_column = .5, prop_required_row = .75
     df.dropna(axis=0, thresh=threshold, inplace=True)
     return df
 
-def clean_wine_data(df):
-    df = df.drop(columns = ['citric acid', 'chlorides'])
-    return df
+def data_prep(df, cols_to_remove=[], prop_required_column=.5, prop_required_row=.75):
+    df = remove_columns(df, cols_to_remove)
+    df = handle_missing_values(df, prop_required_column, prop_required_row)
+    return d
+
+
+def split_wine_data(df, stratify_by=None):
+    """
+    train, validate, test split
+    To stratify, send in a column name
+    """
+    
+    if stratify_by == None:
+        train, test = train_test_split(df, test_size=.2, random_state=123)
+        train, validate = train_test_split(df, test_size=.3, random_state=123)
+    else:
+        train_validate, test = train_test_split(df, test_size=.2, random_state=123, stratify=df[stratify_by])
+        train, validate = train_test_split(train_validate, test_size=.3, random_state=123, stratify=train_validate[stratify_by])
+    
+    return train, validate, test
